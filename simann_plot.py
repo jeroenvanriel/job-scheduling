2dd75fecaa3fba889c4152b9d1cb2da5b1fa29ac
Jeroen van Riel <jeroenvanriel@outlook.com>
2020-11-24 22:40:27 +0100

    Added a function to display a specific assignment with completion time for each machine.

--- simann_plot.py
+++ simann_plot.py
@@ -4,9 +4,38 @@
 import os, sys, pickle
 import matplotlib.pyplot as plt
 
+def printAssignment(data):
+    (best_makespan, best_state, makespan_lst, temperature_lst, difference_lst) = data
+
+    # read a list of the processing times for the jobs
+    ptimes = []
+
+    input_file = open("ptimes.txt", "r")
+    for p in input_file:
+        ptimes.append(int(p))
+
+    n = len(best_state)
+    m = 10
+
+    # For each machine we create a list that contains the jobs that have
+    # been assigned to it.
+    machine_jobs = [[] for x in range(m)]
+    for job in range(n):
+        machine_jobs[best_state[job]].append(job)
+
+    # we print all the jobs that are assigned to each machine
+    for machine in range(m):
+        total = 0
+        for job in machine_jobs[machine]:
+            total += ptimes[job]
+
+        print("Machine {}: (total {}) {}".format(machine, total, machine_jobs[machine]))
+
+    print("Makespan: {:e}".format(best_makespan))
+
 
 def plotData(data):
-    data = (best_makespane, best_state, makespan_lst, temperature_lst, difference_lst)
+    (best_makespan, best_state, makespan_lst, temperature_lst, difference_lst) = data
 
     plt.figure()
 
@@ -33,6 +62,7 @@ if os.path.exists(fn):
         data = pickle.load(fp)
         
         if data is not None:
+            printAssignment(data)
             plotData(data)
         else:
             print("Cannot load data from file.")
