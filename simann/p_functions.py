6a78a4a96ce8a56606ddbd4d7020315b8223f118
Jeroen van Riel <jeroenvanriel@outlook.com>
2020-12-03 18:55:21 +0100

    Made a distinction for null-moves.

--- simann/p_functions.py
+++ simann/p_functions.py
@@ -21,6 +21,17 @@ def linearBadmoveAccept(difference, temperature):
 
 def exponentialDecay(difference, temperature):
     """Use an exponential function of the difference."""
+    if difference < 0: # good-move
+        # If the improvement is too good. We want to make the algorithm less 'greedy'.
+        return random.random() < math.exp(difference / 8e10)
+    else: # bad-move
+        # If the temperature approaches 0, we are getting more careful, so the change
+        # of still accepting gets smaller and smaller.
+        # Furthermore, the larger the difference, the smaller the chance that we will accept the move.
+        return random.random() < temperature * math.exp(-difference / 4e12)
+
+def exponentialDecayNonInc(difference, temperature):
+    """Use an exponential function of the difference."""
     if difference <= 0: # good-move
         # If the improvement is too good. We want to make the algorithm less 'greedy'.
         return random.random() < math.exp(difference / 8e10)
