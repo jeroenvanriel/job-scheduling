927559684618e7f027a0f698cea85a11d76137d5
Jeroen van Riel <jeroenvanriel@outlook.com>
2020-12-03 18:34:34 +0100

    Added a first draft for some experiments.

---
+++ simann/first_comparison.py
@@ -0,0 +1,28 @@
+from simann import SimulatedAnnealing
+from experiment import Experiment
+from p_functions import localSearch, linearBadmoveAccept
+
+# problem defenition
+m = 10 # number of machines
+jobs_file = "../ptimes.txt" # file that contains the jobs
+instance = SimulatedAnnealing.fromFile(jobs_file, m)
+
+# output filename
+output_prefix = "./experiments/exp"
+
+# run experiments and save their result plots to file
+
+exp1 = instance.start(1000, localSearch)
+exp1.plotResults("{}1.png".format(output_prefix))
+
+
+exp2 = instance.start(1000, linearBadmoveAccept)
+exp2.plotResults("{}2.png".format(output_prefix))
+
+
+exp3 = instance.start(10000, localSearch)
+exp3.plotResults("{}3.png".format(output_prefix))
+
+
+exp4 = instance.start(10000, linearBadmoveAccept)
+exp4.plotResults("{}4.png".format(output_prefix))
