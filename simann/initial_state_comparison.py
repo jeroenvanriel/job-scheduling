ce8679506f2ef6cf44f0beb62dbdd5e8ae85e4cc
Sherry <f.m.sherry@student.tue.nl>
2020-12-09 12:17:01 +0100

    Output figures png->pdf, improving text scaling in latex. Fixed (potential) bug initial_state_comparison.py, causing first 2 experiments not to generate figures.

--- simann/initial_state_comparison.py
+++ simann/initial_state_comparison.py
@@ -13,15 +13,15 @@ output_prefix = "./experiments/state_exp"
 # run experiments and save their result plots to file
 
 exp1 = instance.start(100, localSearch, 0, 8e10, 4e12)
-exp1.plotResults("{}1.png".format(output_prefix))
-exp1.plotSchedule(instance, file_name="./experiments/state_exp1_schedule.png")
+exp1.plotResults("{}1.pdf".format(output_prefix))
+exp1.plotSchedule(instance, file_name="./experiments/state_exp1_schedule.pdf")
 
 exp2 = instance.start(100, localSearch, 1, 8e10, 4e12)
-exp2.plotResults("{}2.png".format(output_prefix))
+exp2.plotResults("{}2.pdf".format(output_prefix))
 
 
 exp3 = instance.start(10000, localSearch, 0, 8e10, 4e12)
-exp3.plotResults("{}1.png".format(output_prefix))
+exp3.plotResults("{}3.pdf".format(output_prefix))
 
-exp3 = instance.start(10000, localSearch, 1, 8e10, 4e12)
-exp3.plotResults("{}2.png".format(output_prefix))
+exp4 = instance.start(10000, localSearch, 1, 8e10, 4e12)
+exp4.plotResults("{}4.pdf".format(output_prefix))
