7c4346a0b3c66ed6e93b360ee2f4c3adf3d8f2e7
Jeroen van Riel <jeroenvanriel@outlook.com>
2021-01-11 19:11:36 +0100

    Added list for good/bad ratio, made parameters more accessible, made a plotting function.

--- simann/parameter_comparison.py
+++ simann/parameter_comparison.py
@@ -7,15 +7,16 @@ from matplotlib.gridspec import GridSpec
 from dataclasses import dataclass
 from statistics import mean
 
+# We perform hyperparameter tweaking by doing a grid search on the ranges defined below.
+
 # problem definition
 m = 10 # number of machines
 jobs_file = "../ptimes.txt" # file that contains the jobs
 instance = SimulatedAnnealing.fromFile(jobs_file, m)
 
-# list of lists, for the eventual results in the search grid
+# grid lists, for the eventual results in the search grid
 final_best_makespan_lst = []
-
-# we perform hyperparameter tweaking by doing a grid search on the ranges defined below
+final_ratio_lst = []
 
 # bad-acceptance parameter range
 k_min = 10**8
@@ -27,39 +28,59 @@ i_min = 10**8
 i_step = 2*10**8
 i_n = 25
 
-# loop through the bad-acceptance parameters
+# the number of iterations for each experiment
+n_iterations = 3000
+
+# the number of repeated experiments with the same parameters (so per pixel)
+n_duplicates = 2
+
+
+# loop through the bad-acceptance parameters (y-axis)
 for k in range(k_n):
     best_makespan_lst = []
+    ratio_lst = []
     
-    # loop through the good-acceptance parameter
+    # loop through the good-acceptance parameter (x-axis)
     for i in range(i_n):
         makespan_lst = []
-        for j in range(2):
-            accept_experiment = instance.start(3000, exponentialDecay, 0, i_min+i*i_step, k_min+k*k_step)
-            makespan_lst.append(accept_experiment.best_makespan)
+        ratio = []
+
+        # perform multiple runs at each pixel
+        for j in range(n_duplicates):
+            exp = instance.start(n_iterations, exponentialDecay, 0, i_min+i*i_step, k_min+k*k_step)
+            makespan_lst.append(exp.best_makespan)
+            ratio.append(len(exp.accepted_good_difference_lst) / len(exp.accepted_bad_difference_lst))
+
             print(int((i/i_n)*100)*'#')
             print(int((k/k_n)*100)*'#')
+
         best_makespan_lst.append(mean(makespan_lst))
-        
-        
+        ratio_lst.append(mean(ratio))
+ 
     final_best_makespan_lst.append(best_makespan_lst)
+    final_ratio_lst.append(ratio_lst)
 
 
+def plotGridData(grid_data, image_id):
+    fig = plt.figure(figsize=(8, 8))
+    gs = GridSpec(nrows=1, ncols=1)
+    ax0 = fig.add_subplot(gs[0, 0])
+    plt.title('best makespan')
+    heatmap = ax0.imshow(grid_data, extent=[0,i_n,0,k_n], origin='lower')
+    ax0.set_xticks([ i_n*(1/2 + t)/(i_n) for t in range(i_n)])
+    ax0.set_yticks([ k_n*(1/2 + t)/(k_n) for t in range(k_n)])
+    ax0.set_xticklabels(["{:.1e}".format(i_min+i*i_step) for i in range(i_n)])
+    ax0.set_yticklabels(["{:.1e}".format(k_min+k*k_step) for k in range(k_n)])
+    plt.xlabel("good move acceptance")
+    plt.ylabel("bad move acceptance")
+    plt.colorbar(heatmap)
+    fig.autofmt_xdate()
+    plt.savefig("./experiments/heatmap_exponentialdecay_given_data_zoom" + str(image_id) + ".png")
+    plt.savefig("./experiments/heatmap_exponentialdecay_given_data_zoom" + str(image_id) + ".pdf")
+    plt.show()
 
-fig = plt.figure(figsize=(8, 8))
-gs = GridSpec(nrows=1, ncols=1)
 
-ax0 = fig.add_subplot(gs[0, 0])
-plt.title('best makespan')
-heatmap = ax0.imshow(final_best_makespan_lst, extent=[0,i_n,0,k_n])
-ax0.set_xticks([ i_n*(1/2 + t)/(i_n) for t in range(i_n)])
-ax0.set_yticks([ k_n*(1/2 + t)/(k_n) for t in range(k_n)])
-ax0.set_xticklabels(["{:.1e}".format(i_min+i*i_step) for i in range(i_n)])
-ax0.set_yticklabels(["{:.1e}".format(k_min+k*k_step) for k in range(k_n)])
-plt.xlabel("good move acceptance")
-plt.ylabel("bad move acceptance")
-plt.colorbar(heatmap)
-fig.autofmt_xdate()
-plt.savefig("./experiments/heatmap_exponentialdecay_given_data_zoom.png")
-plt.savefig("./experiments/heatmap_exponentialdecay_given_data_zoom.pdf")
-plt.show()
+plotGridData(final_best_makespan_lst, 0)
+plotGridData(final_ratio_lst, 1)
+
+print(final_ratio_lst)
