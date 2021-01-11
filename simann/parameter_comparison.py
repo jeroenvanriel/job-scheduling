4f604e29096d7a756f7029fb39226d27acab0a28
maxclement684 <m.m.clement@student.tue.nl>
2021-01-11 15:23:04 +0100

    update parameter comparison

--- simann/parameter_comparison.py
+++ simann/parameter_comparison.py
@@ -15,25 +15,26 @@ instance = SimulatedAnnealing.fromFile(jobs_file, m)
 
 
 
-
 final_best_makespan_lst = []
-k_min = 10**9
-k_max = 3*10**10
-k_step = 10**9
-for k in range(k_min, k_max, k_step):
+k_min = 10**8
+k_step = 2*10**9
+k_n = 40
+
+for k in range(k_n):
     best_makespan_lst = []
     
     i_min = 10**8
-    i_max = 5*10**9
-    i_step = 10**8
+    i_step = 2*10**8
+    i_n = 25
 
-    for i in range(i_min, i_max, i_step):
+
+    for i in range(i_n):
         makespan_lst = []
-        for j in range(1):
-            accept_experiment = instance.start(1000, exponentialDecay, 0, i, k)
+        for j in range(2):
+            accept_experiment = instance.start(3000, exponentialDecay, 0, i_min+i*i_step, k_min+k*k_step)
             makespan_lst.append(accept_experiment.best_makespan)
-            
-            print(int((k/k_max)*100)*'#')
+            print(int((i/i_n)*100)*'#')
+            print(int((k/k_n)*100)*'#')
         best_makespan_lst.append(mean(makespan_lst))
         
         
@@ -41,16 +42,16 @@ for k in range(k_min, k_max, k_step):
     
 
 
-fig = plt.figure(figsize=(12, 5))
+fig = plt.figure(figsize=(8, 8))
 gs = GridSpec(nrows=1, ncols=1)
 
 ax0 = fig.add_subplot(gs[0, 0])
 plt.title('best makespan')
-heatmap = ax0.imshow(final_best_makespan_lst, extent=[0,5,0,3])
-ax0.set_xticks([5*t/i_max + 5/(2*i_max) for t in range(i_min, i_max, 2*i_step)])
-ax0.set_yticks([3*t/k_max + 3/(2*k_max) for t in range(k_min, k_max, k_step)])
-ax0.set_xticklabels(["{:.1e}".format(i) for i in range(i_min, i_max, 2*i_step)])
-ax0.set_yticklabels(["{:.1e}".format(k) for k in range(k_min, k_max, k_step)])
+heatmap = ax0.imshow(final_best_makespan_lst, extent=[0,i_n,0,k_n])
+ax0.set_xticks([ i_n*(1/2 + t)/(i_n) for t in range(i_n)])
+ax0.set_yticks([ k_n*(1/2 + t)/(k_n) for t in range(k_n)])
+ax0.set_xticklabels(["{:.1e}".format(i_min+i*i_step) for i in range(i_n)])
+ax0.set_yticklabels(["{:.1e}".format(k_min+k*k_step) for k in range(k_n)])
 plt.xlabel("good move acceptance")
 plt.ylabel("bad move acceptance")
 plt.colorbar(heatmap)
